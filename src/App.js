70f139a5cc365c7e76c11c166a33d33479f3c411
Lady Trell <antrellkent@gmail.com>
2022-06-25 20:56:32 -0400

    contact form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+//import { useState } from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Contact from './components/Contact';
 
 function App() {
+  //const [currentPage, handlePageChange] = useState('About');
+/*
+  const renderPage = () => {
+    // Add a switch statement that will return the appropriate component of the 'currentPage'
+    // YOUR CODE HERE
+    //
+    switch (currentPage) {
+      case "About": return <About />;
+      case "Contact": return <Contact />;    
+      default: return <About />;
+    }
+  };
+
+  */
   return (    
-    <div>
+    <div> 
         <Header></Header>
         <About></About>
         <Contact></Contact>
