4fb2c67bc9b73dddb554b07efdc1f7bff360cb7e
Lady Trell <antrellkent@gmail.com>
2022-06-25 20:10:10 -0400

    contact start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (    
     <div>
         <Header></Header>
         <About></About>
+        <Contact></Contact>
     </div>
   );
 }
