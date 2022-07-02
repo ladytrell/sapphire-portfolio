0d432879fb7217d67425bdd7041186811c7cf7c7
Lady Trell <antrellkent@gmail.com>
2022-07-01 21:32:13 -0400

    portfolio component style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,13 +2,13 @@
 function Footer() {
     //const tabs = ['About', 'Portfolio', 'Resume', 'Contact'];
     return ( 
-      <nav>
+      <footer>
         <ul className="">
             <li><a href="https://github.com/ladytrell"  target="">GitHub</a></li>
             <li><a href="https://www.linkedin.com/in/antrellkent"  target="">LinkedIn</a></li>
             <li><a href="https://stackoverflow.com/users/17929890/antrell-k"  target="">Stack Overflow</a></li>
         </ul>
-      </nav>
+      </footer>
     );
   }
   
