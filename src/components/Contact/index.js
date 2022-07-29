09c7d212b782839c47abf94e49116aa007bc9eed
Lady Trell <antrellkent@gmail.com>
2022-07-28 21:36:10 -0400

    section-bx styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -38,7 +38,7 @@ function ContactForm() {
     }
 
     return (
-        <section>
+        <section className="section-box">
           <h1 data-testid="h1tag">Contact me</h1>
           <form id="contact-form" onSubmit={handleSubmit}>
             <div>
