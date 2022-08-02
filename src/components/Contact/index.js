8daf4258e8de4443f811e50ae8f9d6f04cd06792
Lady Trell <antrellkent@gmail.com>
2022-08-02 18:17:15 -0400

    contact edit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,11 +5,9 @@ function ContactForm() {
     return (
         <section className="section-box">
           <h1 data-testid="h1tag">Contact me</h1>
-          
+          <br></br>
           <p>
-            Feel free to reach out via email 
-            <a href='mailto:antrellkent@gmail.com'>antrellkent@gmail</a> 
-            I look forward to hearing from you.
+            Feel free to reach out via email <a href='mailto:antrellkent@gmail.com'>antrellkent@gmail</a>.  I look forward to hearing from you.
           </p>
         </section>
       );
